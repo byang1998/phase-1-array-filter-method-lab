cc46d96654d392461643a8cd1a659f34bde17663
Ben Yang <benyang2020@gmail.com>
2021-09-04 00:34:08 -0400

    commit tree 6c218efe7577fbf48c8f8269417504608effc12d

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+
+function findMatching(drivers, string){
+
+
+
+const result = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+  return result;
+}
+
+// function fuzzyMatch(drivers, string) {
+//     let splitString = string.split('')
+//  const fuzzyMatches = drivers.filter( driver => {
+//         let splitDriver = driver.split('')
+//         splitDriver[0].toLowerCase() === splitString[0].toLowerCase()
+// })
+//     return fuzzyMatches;
+// }
+
+function fuzzyMatch(drivers, string) {
+    const fuzzyMatches = drivers.filter( driver =>
+      driver.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+    return fuzzyMatches;
+  }
+
+  function matchName(drivers, string) {
+    
+    const match = drivers.filter(driver => driver.name === string)
+  return match;
+}
+
+  
